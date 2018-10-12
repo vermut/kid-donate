3c320b7ed604fcaab387383338a5fb832b785907
Pavel Veretennikov <vermut@kid.lv>
2018-10-12 21:09:31 +0300

    assets and shit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,16 +24,14 @@ export class AppComponent {
 
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      // this.statusBar.styleDefault();
+      // this.splashScreen.hide();
     });
   }
 }
